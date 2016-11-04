ebae8e23527eaaaf1b665f37b47e561af7a9144e
victorG <victor.gourbiliere@viacesi.fr>
2016-11-04 11:03:26 +0100

    changer le code source dans code blocks

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+int addition (int a, int b)
+{
+    return a+b;
+}
 int main()
 {
-    printf("Hello world!\n");
+    printf("l'addition de a+b = %d\n", addition(1,4));
     return 0;
 }
